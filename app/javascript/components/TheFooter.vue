75642d788d3ce2f846f818a3851fb0ec79136408
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-06-01 23:03:01 +0900

    Add:フッターの追加

---
+++ app/javascript/components/TheFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="footer items-center p-4 bg-neutral text-neutral-content">
+    <div class="items-center grid-flow-col">
+      <p>© 2022. Oshi-Concept - All right reserved</p>
+    </div>
+    <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+      <a href="#">利用規約</a>
+      <a href="#">プライバシーポリシー</a>
+      <a href="#">お問合わせ</a>
+    </div>
+  </footer>
+</template>
