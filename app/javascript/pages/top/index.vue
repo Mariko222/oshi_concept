c37758ecc7eafe5f61a3a18b725469ffec998664
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-06-01 21:49:54 +0900

    Add:Vue Routerの導入

---
+++ app/javascript/pages/top/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="flex">
+      <div class="rounded border bg-yellow-500 p-3">1</div>
+      <div class="rounded border bg-yellow-500 p-3">2</div>
+      <div class="rounded border bg-yellow-500 p-3">3</div>
+    </div>
+    <button class="btn btn-circle">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+    </button>
+    <button class="btn btn-circle btn-outline">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopIndex",
+}
+</script>
+
+<style scoped>
+</style>
