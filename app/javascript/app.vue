0beb9a49167694f9793c6fbc711a6d1c2a20a924
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-06-03 00:25:36 +0900

    Add:ヘッダーの追加

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <body class="flex flex-col min-h-screen">
-      <router-view />
+      <TheHeader class="mb-auto" />
+        <router-view />
       <TheFooter class="mt-auto" />
     </body>
   </div>
 </template>
 
 <script>
+import TheHeader from "components/TheHeader"
 import TheFooter from "components/TheFooter"
 export default {
   components: {
+    TheHeader,
     TheFooter
   }
 }
