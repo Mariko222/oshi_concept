110555ebc8607a6ba3c94234a93df13406bef8d9
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-06-11 18:23:17 +0900

    Add:Userモデル作成

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 
 import TopIndex from "../pages/top/index";
+import RegisterIndex from "../pages/register/index";
 
 Vue.use(Router)
 
@@ -13,7 +14,12 @@ const router = new Router({
       component: TopIndex,
       name: "TopIndex",
 
-    }
+    },
+    {
+      path: '/register',
+      component: RegisterIndex,
+      name: "RegisterIndex"
+    },
   ]
 })
 
