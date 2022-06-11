db327d43d102282d875d7f2847f86744e6a6280e
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-06-11 20:02:56 +0900

    Add:vuex導入

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
-import Router from "vue-router";
+import VueRouter from "vue-router";
+import store from "../store";
 
 import TopIndex from "../pages/top/index";
 import RegisterIndex from "../pages/register/index";
 
 Vue.use(Router)
 
-const router = new Router({
+const router = new VueRouter({
   mode: "history",
   routes: [
     {
@@ -22,5 +23,16 @@ const router = new Router({
     },
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  store.dispatch('users/fetchAuthUser')
+    .then((authUser) => {
+      if (to.matched.some(record => record.meta.requiredAuth) && !authUser) {
+        next({ name: 'LoginIndex' });
+      } else {
+        next();
+      }
+    })
+});
 
 export default router
