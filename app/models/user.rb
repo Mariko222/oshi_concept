4688c0a0aa57fda17e9b88f0901bc6e098eaaab8
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-06-14 19:11:10 +0900

    Add:mypage_nameカラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@
 #  id               :bigint           not null, primary key
 #  crypted_password :string
 #  email            :string           not null
+#  mypage_name      :integer
 #  name             :string           not null
 #  salt             :string
 #  created_at       :datetime         not null
@@ -24,5 +25,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, length: { maximum: 50 }
+  validates :mypage_name, length: { maximum: 50 }
 
 end
