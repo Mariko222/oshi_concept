a4230eb2a3fbb55ed398fc749c50d95d6184e038
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-06-29 21:20:48 +0900

    Add:微修正とマイページTOP画面にユーザーネームを表示

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,10 @@
 class Api::SessionsController < ApplicationController
 
+  def index
+    @user = User.find(current_user.id)
+    render json: @user
+  end
+
   def create
     user = User.authenticate(params[:email], params[:password])
     if user
