9a09db0dcbbbc4514f58d45d0fe60de977465111
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-07-03 13:40:18 +0900

    Fix:マイページTOP画面レイアウト修正

--- app/javascript/components/MypageHeader.vue
+++ app/javascript/components/MypageHeader.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="w-full lg:block bg-gray-50 border rounded-lg shadow-sm sticky top-20 z-10">
-    <div class="max-w-auto gap-8 p-8">
-      <div class="grid grid-cols-2">
-        <div class="icon mr-5">
-            <img :src="authUser.icon_url" class="rounded-full icon-image" />
+    <div class="max-w-auto gap-8 p-4 pb-1">
+      <div class="grid grid-cols-3">
+        <div class="icon mr-5 flex justify-center items-center">
+            <img :src="authUser.icon_url" class="rounded-full icon-image "  width="150px" />
         </div>
-        <ul class="flex flex-col bg-gray-100 rounded-lg gap-4 p-4 md:p-8">
-          <li class="font-semibold mb-4 text-2xl">{{ user.name }}</li>
-          <li class="font-semibold mb-1">推し：</li>
-          <div v-for="mygenreFavoriteCharacter in mygenreFavoriteCharacters" class="bg-white border shadow-sm rounded p-2">
-            <p>{{ mygenreFavoriteCharacter.character.name }}</p>
-          </div>
-        </ul>
+        <div class="flex flex-col bg-gray-100 rounded-lg gap-2 md:p-5">
+          <p class="font-semibold mb-4 text-2xl">{{ user.name }}</p>
+          <p class="font-semibold mb-1">推し：</p>
+          <ul v-for="mygenreFavoriteCharacter in mygenreFavoriteCharacters" class="bg-white border shadow-sm rounded p-2">
+            <li>{{ mygenreFavoriteCharacter.character.name }}</li>
+          </ul>
+        </div>
       </div>
     </div>
-    <div class="flex flex-col border-t border-b my-5"></div>
+    <div class="flex flex-col border-t border-b"></div>
       <div class="max-w-full gap-4 p-4 mx-auto">
         <div class="justify-between grid grid-cols-2 gap-8">
-          <div class="flex-row justify-start gap-2.5 ml-8">
+          <div class="flex-row justify-start gap-2.5 ml-8  flex justify-center items-center">
             <router-link :to="{ name: 'MypageNew' }" button class="btn bg-indigo-800 text-white btn-outline mr-5">ジャンルを追加</router-link>
-            <div v-for="mygenreList in mygenreLists" :key=" mygenreList.id" class="bg-white border shadow-sm rounded p-2">
-              <p>{{ mygenreList.mygenre.genre.name }}</p>
+            <div class="flex flex-col gap-2">
+              <div v-for="mygenreList in mygenreLists" :key=" mygenreList.id" class="bg-white border shadow-sm rounded p-2">
+                <p>{{ mygenreList.mygenre.genre.name }}</p>
+              </div>
             </div>
           </div>
         </div>
