60e982d4884cbb63aa91efd4a9334ceaf89647c7
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-07-05 18:33:38 +0900

    Add:postsテーブルとPostモデル作成

---
+++ app/models/post.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  category   :integer          default("fashion"), not null
+#  type       :integer          default("twitter"), not null
+#  url        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  mygenre_id :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_mygenre_id  (mygenre_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (mygenre_id => mygenres.id)
+#
+class Post < ApplicationRecord
+  belongs_to :mygenre
+
+  enum category: { fashion: 0, item: 1, place: 2}
+  enum type: { twitter: 0, webpage: 1}
+
+  validates :url, length: { maximum: 200}
+end
