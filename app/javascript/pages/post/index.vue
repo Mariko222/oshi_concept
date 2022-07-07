1334900e8edaba3a0007e2e715f83d302158f3f6
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-07-07 19:32:49 +0900

    Fix:レイアウト変更

--- app/javascript/pages/post/index.vue
+++ app/javascript/pages/post/index.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="container mx-auto">
+  <div>
     <MypageHeader v-if="authUser" class="mb-auto" />
-    <div class="bg-white py-6 sm:py-8 lg:py-12">
-      <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
-        <div class="flex justify-between items-end gap-4 mb-6">
-          <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold">概念</h2>
-        </div>
-        <div v-for="tweetUrl in tweetUrls" class="flex items-end gap-2">
-          <Tweet :id="tweetUrl"></Tweet>
-        </div>
-        <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 md:gap-x-6 gap-y-8">
-          <div>
-            <a href="#" class="group h-80 block bg-gray-100 rounded-lg overflow-hidden relative mb-2 lg:mb-3">
-            </a>
+    <div class="container mx-auto">
+      <div class="bg-white py-6 sm:py-8 lg:py-12">
+        <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold mb-5">概念</h2>
+        <div class="w-full">
+          <div class="flex flex-wrap justify-between gap-4 mb-6">
+            <div class="columns-2 md:columns-3 lg:columns-4">
+              <div v-for="tweetUrl in tweetUrls">
+                <Tweet :id="tweetUrl" class="mb-4"></Tweet>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      </div>
     </div>
   </div>
 </template>
