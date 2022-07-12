3141b06c69617deb6418068c222ca1cee637798e
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-07-12 21:26:51 +0900

    Add: ユーザー新規登録とログインページのバリデーション実装

--- app/javascript/pages/register/index.vue
+++ app/javascript/pages/register/index.vue
@@ -2,66 +2,89 @@
   <div class="bg-white py-6 sm:py-8 lg:py-12">
     <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
       <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-8">新規登録</h2>
+      <ValidationObserver v-slot="ObserverProps">
+        <form class="max-w-lg border rounded-lg mx-auto">
+          <div class="flex flex-col gap-4 p-4 md:p-8">
+            <ValidationProvider name="ユーザーネーム" rules="required">
+              <div slot-scope="ProviderProps">
+                <label for="name" class="inline-block text-gray-800 text-sm sm:text-base mb-2">ユーザーネーム</label>
+                <input id="name" type="text" v-model="user.name" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+                <span class="text-red-500 text-center">{{
+                  ProviderProps.errors[0]
+                }}</span>
+              </div>
+            </ValidationProvider>
 
-      <form class="max-w-lg border rounded-lg mx-auto">
-        <div class="flex flex-col gap-4 p-4 md:p-8">
-          <div>
-            <label for="name" class="inline-block text-gray-800 text-sm sm:text-base mb-2">ユーザーネーム</label>
-            <input id="name" type="text" v-model="user.name" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
-          </div>
+            <ValidationProvider name="メールアドレス" rules="required|email">
+              <div slot-scope="ProviderProps">
+                <label for="email" class="inline-block text-gray-800 text-sm sm:text-base mb-2">メールアドレス</label>
+                <input id="email" type="email" v-model="user.email" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+                <span class="text-red-500 text-center">{{
+                  ProviderProps.errors[0]
+                }}</span>
+              </div>
+            </ValidationProvider>
 
-          <div>
-            <label for="email" class="inline-block text-gray-800 text-sm sm:text-base mb-2">メールアドレス</label>
-            <input id="email" type="email" v-model="user.email" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
-          </div>
+            <ValidationProvider name="パスワード" rules="required">
+              <div slot-scope="ProviderProps">
+                <label for="password" class="inline-block text-gray-800 text-sm sm:text-base mb-2">パスワード</label>
+                <input id="password" type="password" v-model="user.password" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+                <span class="text-red-500 text-center">{{
+                  ProviderProps.errors[0]
+                }}</span>
+              </div>
+            </ValidationProvider>
 
-          <div>
-            <label for="password" class="inline-block text-gray-800 text-sm sm:text-base mb-2">パスワード</label>
-            <input id="password" type="password" v-model="user.password" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
-          </div>
+            <ValidationProvider name="パスワード（確認用）" rules="required|min:3|confirmed">
+              <div slot-scope="ProviderProps">
+                <label for="password_confirmation" class="inline-block text-gray-800 text-sm sm:text-base mb-2">パスワード（確認用）</label>
+                <input id="password_confirmation" type="password" v-model="user.password_confirmation" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+                <span class="text-red-500 text-center">{{
+                  ProviderProps.errors[0]
+                }}</span>
+              </div>
+            </ValidationProvider>
 
-          <div>
-            <label for="password_confirmation" class="inline-block text-gray-800 text-sm sm:text-base mb-2">パスワード（確認用）</label>
-            <input id="password_confirmation" type="password" v-model="user.password_confirmation" class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
+            <button
+              type="button"
+              class="block bg-gray-800 hover:bg-gray-700 active:bg-gray-600 focus-visible:ring ring-gray-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+              :disabled="ObserverProps.invalid || !ObserverProps.validated"
+              @click="register"
+            >
+              登録
+            </button>
+            <span class="text-red-500 text-center">{{ errorMessage }}</span>
+            <div class="flex justify-center items-center relative">
+              <span class="h-px bg-gray-300 absolute inset-x-0"></span>
+              <span class="bg-white text-gray-400 text-sm relative px-4">or</span>
+            </div>
+            <button
+              class="flex justify-center items-center bg-twitter hover:bg-sky-400 active:bg-sky-500 focus-visible:ring ring-blue-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 gap-2 px-8 py-3"
+            >
+              <svg class="w-5 h-5 shrink-0" width="24" height="24" viewBox="328 355 335 276" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="
+                  M 630, 425
+                  A 195, 195 0 0 1 331, 600
+                  A 142, 142 0 0 0 428, 570
+                  A  70,  70 0 0 1 370, 523
+                  A  70,  70 0 0 0 401, 521
+                  A  70,  70 0 0 1 344, 455
+                  A  70,  70 0 0 0 372, 460
+                  A  70,  70 0 0 1 354, 370
+                  A 195, 195 0 0 0 495, 442
+                  A  67,  67 0 0 1 611, 380
+                  A 117, 117 0 0 0 654, 363
+                  A  65,  65 0 0 1 623, 401
+                  A 117, 117 0 0 0 662, 390
+                  A  65,  65 0 0 1 630, 425
+                  Z"
+                  fill="white" />
+              </svg>
+              Twitterでログイン
+            </button>
           </div>
-
-          <button
-            type="button"
-            class="block bg-gray-800 hover:bg-gray-700 active:bg-gray-600 focus-visible:ring ring-gray-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
-            @click="register"
-          >
-            登録
-          </button>
-          <div class="flex justify-center items-center relative">
-            <span class="h-px bg-gray-300 absolute inset-x-0"></span>
-            <span class="bg-white text-gray-400 text-sm relative px-4">or</span>
-          </div>
-          <button
-            class="flex justify-center items-center bg-twitter hover:bg-sky-400 active:bg-sky-500 focus-visible:ring ring-blue-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 gap-2 px-8 py-3"
-          >
-            <svg class="w-5 h-5 shrink-0" width="24" height="24" viewBox="328 355 335 276" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="
-                M 630, 425
-                A 195, 195 0 0 1 331, 600
-                A 142, 142 0 0 0 428, 570
-                A  70,  70 0 0 1 370, 523
-                A  70,  70 0 0 0 401, 521
-                A  70,  70 0 0 1 344, 455
-                A  70,  70 0 0 0 372, 460
-                A  70,  70 0 0 1 354, 370
-                A 195, 195 0 0 0 495, 442
-                A  67,  67 0 0 1 611, 380
-                A 117, 117 0 0 0 654, 363
-                A  65,  65 0 0 1 623, 401
-                A 117, 117 0 0 0 662, 390
-                A  65,  65 0 0 1 630, 425
-                Z"
-                fill="white" />
-            </svg>
-            Twitterでログイン
-          </button>
-        </div>
-      </form>
+        </form>
+      </ValidationObserver>
     </div>
   </div>
 </template>
@@ -78,6 +101,7 @@ export default {
         password: "",
         password_confirmation: "",
       },
+      errorMessage: "",
     };
   },
   methods: {
@@ -89,6 +113,7 @@ export default {
         })
         .catch((err) => {
           console.log(err)
+          this.errorMessage = error.response.data.errors.detail;
         })
     },
   }
