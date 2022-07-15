ef17f32295d46d765381eefd6d2f031e5178acb2
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-07-15 21:46:14 +0900

    Add:ユーザー登録ページのフラッシュメッセージ設定

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -2,6 +2,7 @@
   <div>
     <body class="flex flex-col min-h-screen">
       <TheHeader class="mb-auto" />
+      <TheFlashMessage v-if="isFlash" />
       <router-view />
       <TheFooter class="mt-auto" />
     </body>
@@ -11,14 +12,17 @@
 <script>
 import TheHeader from "components/TheHeader"
 import TheFooter from "components/TheFooter"
+import TheFlashMessage from "components/TheFlashMessage"
 import { mapGetters } from "vuex"
 import axios from "./plugins/axios";
 export default {
   components: {
     TheHeader,
-    TheFooter
+    TheFooter,
+    TheFlashMessage,
   },
   computed: {
+    ...mapGetters(["isFlash"]),
     ...mapGetters("users", ["authUser"])
   }
 }
