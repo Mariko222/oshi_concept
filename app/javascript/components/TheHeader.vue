be18bcda26094e08491f591f179ee086c7f120f6
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-07-22 22:04:30 +0900

    Fix:フォント変更

--- app/javascript/components/TheHeader.vue
+++ app/javascript/components/TheHeader.vue
@@ -75,8 +75,8 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Yomogi&display=swap');
 .page-font {
-  font-family: 'Yusei Magic', sans-serif;
+  font-family: 'Yomogi', cursive;
 }
 </style>
