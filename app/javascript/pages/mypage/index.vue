231046180e2163b6a9d825e87dd1a420c86e8060
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-07-22 21:22:04 +0900

    Fix:フラッシュメッセージの表示位置調整

--- app/javascript/pages/mypage/index.vue
+++ app/javascript/pages/mypage/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <MypageHeader v-if="authUser" class="mb-auto -mt-64" />
-    <div class="container mx-auto">
+    <MypageHeader v-if="authUser" class="mb-auto" />
+    <div class="container mx-auto mt-60">
       <div class="bg-purple-100 py-6 sm:py-8 lg:py-12">
         <div class="grid grid-cols-3">
           <div class="page-font lg:flex flex-col sm:flex-row gap-2.5 ml-8 flex justify-center items-center bg-lime-500 hover:bg-lime-400 active:bg-lime-600 text-white rounded-lg py-3">
