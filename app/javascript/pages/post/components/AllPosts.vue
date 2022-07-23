bb0af3898d49645fc29793595e33f29f0b63db42
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-07-23 12:12:34 +0900

    Add:ツイート表示までの間のローディングアニメーション追加

--- app/javascript/pages/post/components/AllPosts.vue
+++ app/javascript/pages/post/components/AllPosts.vue
@@ -9,7 +9,7 @@
         <div class="columns-2 md:columns-3 lg:columns-4">
           <div v-for="tweet in tweets">
             <div class="card bg-violet-400 px-2 py-2 -ml-2 mb-3">
-              <Tweet :id="tweet.tweet_url"></Tweet>
+              <Tweet :id="tweet.tweet_url"><div class="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div></Tweet>
                 <button type="button" @click="handleDeleteTweet(tweet)">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-right" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -66,21 +66,29 @@ export default {
     }
   },
   created() {
-    this.fetchPosts()
-  },
-  mounted() {
-    this.fetchPosts();
+    this.fetchBoth()
   },
   methods: {
-    fetchPosts() {
+    fetchBoth: function () {
+      this.fetchTweets();
+      this.fetchWebpages();
+    },
+    fetchTweets() {
+      this.$axios.get("posts")
+        .then(res => {
+          this.posts = res.data
+          this.tweets = this.posts.filter(p =>{
+            return p.type === "twitter"
+          })
+        })
+        .catch(err => console.log(err.status));
+    },
+    fetchWebpages() {
       this.$axios.get("posts")
         .then(res => {
           this.posts = res.data
           this.webpages = this.posts.filter(p =>{
             return p.type === "webpage"
-          })
-          this.tweets = this.posts.filter(p =>{
-            return p.type === "twitter"
           })
         })
         .catch(err => console.log(err.status));
