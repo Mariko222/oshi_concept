b90a44f661e992e3d1cbccc1762a4d2d5a06f6eb
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-01 10:51:02 +0900

    Add:CSRF対策コード追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   include Api::ExceptionHandler
   include Api::UserAuthenticator
+  # protect_from_forgeryを有効化
+  include ActionController::RequestForgeryProtection
+  # APiモードでのCookieを使用するために読み込む
+  include ActionController::Cookies
 
   protect_from_forgery with: :null_session
+
+  private
+
+  def set_csrf_token
+    cookies['CSRF-TOKEN'] = {
+      domain: Settings.default_url_options.cookie_host,
+      value: form_authenticity_token
+    }
+  end
 end
