7fecbae6c32b840b21c6c17f09ae7dddb1db6f03
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-03 22:47:48 +0900

    Fix:レイアウト修正

--- app/javascript/components/MypageHeader.vue
+++ app/javascript/components/MypageHeader.vue
@@ -1,13 +1,43 @@
 <template>
-  <div class="w-full lg:block bg-violet-300 border shadow-sm sticky top-20 z-10">
-    <div class="max-w-auto gap-8 p-4 pb-1">
+  <div class="w-full bg-violet-300 sticky top-20 z-10">
+    <div class="max-w-auto p-4 pb-1">
       <div class="grid grid-cols-3">
-        <div class="icon mt-10 mr-5 flex jflex flex-col flex justify-center items-center">
-            <img :src="authUser.icon_url" class="rounded-full icon-image"/>
-            <p class="page-font mt-5 mb-2 text-2xl">{{ authUser.name }}</p>
+        <div class="icon sm:-pt-6 lg:-mt-8 pb-5 mr-5 flex jflex flex-col flex justify-center items-center">
+          <img :src="authUser.icon_url" class="rounded-full icon-image lg:mt-12"/>
+          <ul class="justify-center items-center md:flex">
+            <li>
+              <router-link :to="{ name: 'MypageNew' }" class="page-font btn bg-fuchsia-500 hover:bg-fuchsia-400 active:bg-fuchsia-600 rounded-lg transition duration-100 mt-8 mr-3">ジャンルを追加</router-link>
+            </li>
+            <li>
+              <button
+                class="page-font flex justify-center bg-twitter hover:bg-sky-400 active:bg-sky-500 text-white font-semibold text-center rounded-lg outline-none transition duration-100 gap-2 mt-8 px-2 py-3"
+              >
+                <svg class="w-5 h-5 shrink-0" width="24" height="24" viewBox="328 355 335 276" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="
+                    M 630, 425
+                    A 195, 195 0 0 1 331, 600
+                    A 142, 142 0 0 0 428, 570
+                    A  70,  70 0 0 1 370, 523
+                    A  70,  70 0 0 0 401, 521
+                    A  70,  70 0 0 1 344, 455
+                    A  70,  70 0 0 0 372, 460
+                    A  70,  70 0 0 1 354, 370
+                    A 195, 195 0 0 0 495, 442
+                    A  67,  67 0 0 1 611, 380
+                    A 117, 117 0 0 0 654, 363
+                    A  65,  65 0 0 1 623, 401
+                    A 117, 117 0 0 0 662, 390
+                    A  65,  65 0 0 1 630, 425
+                    Z"
+                    fill="white" />
+                </svg>
+                Twitterで共有
+              </button>
+            </li>
+          </ul>
         </div>
-        <div class="bg-violet-200 rounded-lg gap-2 md:p-5 mb-3">
-          <p class="page-font text-xl mb-1">推し：</p>
+        <div class="bg-violet-200 my-auto m-2 p-3 rounded-lg">
+          <p class="page-font mb-1 sm:text-base lg:text-xl"><span class="page-font">{{ authUser.name }}</span>の推し：</p>
           <div class="flex flex-wrap justify-between">
             <p class="page-font" v-if="mygenreCharacters.length === 0">登録済みのジャンルリストからジャンルを選んでください。</p>
             <ul v-for="mygenreCharacter in mygenreCharacters" class="rounded p-2">
@@ -15,45 +45,13 @@
             </ul>
           </div>
         </div>
-        <div class="border-l-2 ml-5 mb-3 border-violet-500 ...">
-          <div class="flex jflex flex-col gap-2.5 flex justify-between items-center">
-            <div class="flex flex-col gap-2">
-              <div v-for="mygenre in mygenres" :key=" mygenre.id" class="bg-violet-300">
-                <button class="page-font text-white bg-violet-500 border rounded p-2" @click="fetchBoth(mygenre)">{{ mygenre.genre.name }}</button>
-              </div>
+        <div class="flex flex-col my-auto items-center">
+          <p class="page-font sm:text-base lg:text-xl mb-3">登録済みのジャンルリスト:</p>
+          <p class="page-font" v-if="mygenres.length === 0">ジャンルを追加してください</p>
+          <div class="flex flex-col flex justify-between items-center">
+            <div v-for="mygenre in mygenres" :key="mygenre.id" class="bg-violet-300">
+              <button class="page-font text-white bg-violet-500 border rounded p-2" @click="fetchBoth(mygenre)">{{ mygenre.genre.name }}</button>
             </div>
-          </div>
-            <ul class="flex flex-row justify-center items-center">
-              <li>
-                <router-link :to="{ name: 'MypageNew' }" class="page-font btn bg-fuchsia-500 hover:bg-fuchsia-400 active:bg-fuchsia-600 rounded-lg transition duration-100 mt-8 mr-3">ジャンルを追加</router-link>
-              </li>
-              <li>
-                <button
-                  class="page-font flex justify-center bg-twitter hover:bg-sky-400 active:bg-sky-500 text-white font-semibold text-center rounded-lg outline-none transition duration-100 gap-2 mt-8 px-2 py-3"
-                >
-                  <svg class="w-5 h-5 shrink-0" width="24" height="24" viewBox="328 355 335 276" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="
-                      M 630, 425
-                      A 195, 195 0 0 1 331, 600
-                      A 142, 142 0 0 0 428, 570
-                      A  70,  70 0 0 1 370, 523
-                      A  70,  70 0 0 0 401, 521
-                      A  70,  70 0 0 1 344, 455
-                      A  70,  70 0 0 0 372, 460
-                      A  70,  70 0 0 1 354, 370
-                      A 195, 195 0 0 0 495, 442
-                      A  67,  67 0 0 1 611, 380
-                      A 117, 117 0 0 0 654, 363
-                      A  65,  65 0 0 1 623, 401
-                      A 117, 117 0 0 0 662, 390
-                      A  65,  65 0 0 1 630, 425
-                      Z"
-                      fill="white" />
-                  </svg>
-                  Twitterで共有
-                </button>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
--- app/javascript/pages/post/components/ItemPosts.vue
+++ app/javascript/pages/post/components/ItemPosts.vue
@@ -4,13 +4,13 @@
       <div v-if="itemTweets.length == 0  && itemWebpages.length == 0">
         <p class="page-font">雑貨カテゴリの投稿がありません</p>
       </div>
-      <div class="flex flex-wrap justify-between gap-4 mb-6">
-        <div class="columns-2 md:columns-3 lg:columns-4">
+      <div class="flex flex-wrap justify-between mx-12 my-3 lg:mx-3 lg:my-3">
+        <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           <div v-for="itemTweet in itemTweets">
-            <div class="card bg-violet-400 px-2 py-2 -ml-2 mb-3">
+            <div class="card bg-neutral px-2 py-2 m-3">
               <Tweet :id="itemTweet.tweet_url"><div class="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div></Tweet>
                 <button type="button" @click="handleDeleteTweet(itemTweet)">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-right" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-right text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
                 </button>
@@ -19,10 +19,10 @@
         </div>
       </div>
 
-      <div class="flex flex-wrap justify-between gap-4 mb-6">
-        <div class="grid sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-6 xl:gap-8">
+      <div class="flex flex-wrap justify-between mb-6">
+        <div class="grid sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           <div v-for="itemWebpage in itemWebpages">
-            <div class="card bg-violet-400 px-2 py-2 -ml-2 mb-3">
+            <div class="card bg-neutral px-2 py-2 ">
               <div class="bg-white border rounded-lg overflow-hidden">
                 <a :href="itemWebpage.url" class="group h-48 md:h-64 block bg-gray-100 overflow-hidden relative">
                   <img src="../../../../../public/img/webpage.jpg" v-if="!itemWebpage.image" loading="lazy" alt="NO_IMAGE" class="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
@@ -35,7 +35,7 @@
                 </div>
               </div>
               <button type="button" @click="handleDeleteWebpage(itemWebpage)" class="mt-2">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-right" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-right text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
               </button>
