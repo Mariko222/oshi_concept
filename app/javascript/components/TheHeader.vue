05fb8f3e7e5fdc29b8954c95682b5102e9db8e79
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-05 22:32:28 +0900

    Fix:ボタンの形変更などレイアウト修正

--- app/javascript/components/TheHeader.vue
+++ app/javascript/components/TheHeader.vue
@@ -11,7 +11,7 @@
     <div class="navbar-end">
       <div class="flex-none">
         <div class="dropdown dropdown-end">
-          <button class="btn btn-square btn-ghost">
+          <button class="btn btn-square btn-ghost" tabindex="0">
             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
               <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/>
             </svg>
