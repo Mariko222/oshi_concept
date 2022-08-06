ba12292693d6faa2ee5986b4f3aeacac81b36a4f
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-06 13:12:51 +0900

    Add:サービスの利用説明追加

--- app/javascript/pages/top/index.vue
+++ app/javascript/pages/top/index.vue
@@ -5,17 +5,31 @@
         <section class="flex flex-col items-center">
           <div class="max-w-xl flex flex-col items-center text-center pt-8 lg:pt-32 pb-0 sm:pb-16 lg:pb-32">
             <p class="page-font text-black-800 lg:text-6xl text-3xl font-bold mb-8 md:mb-12">推し<span class="page-font text-indigo-500 lg:text-6xl text-3xl font-bold">概念</span>集めてみた</p>
-            <p class="page-font text-purple-500 text-xl mb-4 md:mb-6">いいなと思う情報を見つけて保存しておいても、いざ欲しい時に探すのに苦労した経験はありませんか？</p>
+            <p class="page-font text-purple-500 text-xl mb-4 md:mb-6">TwitterやWEBページでいいなと思う情報を見つけて保存しても、いざ欲しいとき他の情報に埋もれてしまって探すのに苦労した経験はありませんか？</p>
             <div class="bg-gray-100 rounded-lg shadow-lg grid  gap-8 lg:gap-12 mt-10 mb-10">
               <div>
                 <div class="h-80 md:h-auto overflow-hidden rounded-lg">
-                  <img img src="../../../../public/img/mypage.jpg" loading="lazy" alt="mypage-top" class="pl-2 pr-2 my-10" />
+                  <img img src="../../../../public/img/post_show.jpg" loading="lazy" alt="mypage-top" class="pl-2 pr-2 my-10" />
                 </div>
               </div>
               <div class="text-center pb-12">
                 <p class="page-font text-gray-800 mb-4">“推し概念集めてみた”を使うと、</p>
-                <p class="page-font text-gray-800 mb-4 ml-3">ジャンルと推しを選んで概念ツイートやサイトのURLを保存して、自分が”概念”と思ったものをひたすら集めてオリジナルのマイページを作成できる</p>
+                <p class="page-font text-gray-800 mb-4 ml-3">ジャンルごとに推しっぽいと思ったツイートやWEBページを保存して<br>自分が”概念”と感じたものを好きなだけ集めて<br>オリジナルのマイページを作成できます</p>
               </div>
+            </div>
+            <div class="bg-gray-100 rounded-lg shadow-lg grid  gap-8 lg:gap-12 mt-10 mb-10">
+              <div>
+                <p class="page-font text-xl mt-5 text-purple-500">使い方は簡単！</p>
+                <div class="h-80 md:h-auto overflow-hidden rounded-lg">
+                  <img img src="../../../../public/img/how_to_use.jpg" loading="lazy" alt="mypage-top" class="pl-2 pr-2 my-10" />
+                </div>
+              </div>
+              <div class="text-center pb-12">
+                <p class="page-font text-gray-800 mb-4">STEP1:マイページ名とアイコンを登録</p>
+                <p class="page-font text-gray-800 mb-4 ml-3">STEP2:ジャンルと推しを登録</p>
+                <p class="page-font text-gray-800 mb-4 ml-3">STEP3:推しっぽいものを見つけたらURLを投稿</p>
+              </div>
+
             </div>
             <div class="text-gray-500 xl:text-lg leading-relaxed mb-8 md:mb-12">
               <p class="page-font text-xl mb-8">このピアス推しっぽい</p>
