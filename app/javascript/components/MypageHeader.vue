a59af623f3aa68d2e2e0bf1d1298d1744aeddb88
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-06 09:16:59 +0900

    Fix:スマホでのレイアウト崩れ修正

--- app/javascript/components/MypageHeader.vue
+++ app/javascript/components/MypageHeader.vue
@@ -14,7 +14,7 @@
           <ul class="justify-center items-center md:flex">
             <li>
               <button
-                class="page-font text-sm flex justify-center bg-twitter hover:bg-sky-400 active:bg-sky-500 text-white font-semibold text-center rounded-full transition duration-100 gap-2 mt-3 px-2 py-2"
+                class="page-font text-xs flex justify-center bg-twitter hover:bg-sky-400 active:bg-sky-500 text-white font-semibold text-center rounded-full transition duration-100 gap-2 mt-3 px-2 py-2"
               >
                 <svg class="w-5 h-5 shrink-0" width="24" height="24" viewBox="328 355 335 276" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="
@@ -42,7 +42,7 @@
         </div>
         <div>
           <div class="bg-violet-200 h-full -mt-3 mr-1 px-2 py-3 rounded-lg">
-            <p class="page-font mb-1 text-sm lg:text-base"><span class="page-font">{{ authUser.name }}</span>の推し：</p>
+            <p class="page-font mb-1 text-xs lg:text-base"><span class="page-font">{{ authUser.name }}</span>の推し：</p>
             <router-link :to="{ name: 'MygenresEdit' }" class="nav-link">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-indigo-800" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
@@ -50,27 +50,31 @@
               </svg>
             </router-link>
             <div class="flex flex-wrap justify-between">
-              <p class="page-font text-sm lg:text-base" v-if="mygenreCharacters.length === 0">登録したジャンルを選んでください。</p>
+              <p class="page-font text-xs lg:text-base" v-if="mygenreCharacters.length === 0">登録したジャンルを選んでください。</p>
               <ul v-for="mygenreCharacter in mygenreCharacters" class="rounded p-2">
-                <li class="page-font text-sm lg:text-xl">{{ mygenreCharacter.character.name }}</li>
+                <li class="page-font text-xs lg:text-xl">{{ mygenreCharacter.character.name }}</li>
               </ul>
             </div>
           </div>
         </div>
         <div class="flex flex-col my-auto items-center">
           <div class="flex">
-            <p class="page-font text-sm lg:text-base mb-3 -mt-6"><span class="page-font text-sm lg:text-base">{{ authUser.name }}</span>のジャンルリスト:</p>
+            <p class="page-font text-xs lg:text-base mb-3 -mt-6"><span class="page-font text-xs lg:text-base">{{ authUser.name }}</span>のジャンルリスト:</p>
             <router-link :to="{ name: 'MypageNew' }" class="nav-link">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-indigo-800" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-              </svg>
+              <button
+                class="w-5 h-5 bg-purple-700 rounded-full hover:bg-purple-800 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
+                <svg viewBox="0 0 20 30" enable-background="new 0 0 20 20" class="w-5 h-5 inline-block">
+                  <path fill="#FFFFFF" d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
+                                          C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
+                                          C15.952,9,16,9.447,16,10z" />
+                </svg>
+              </button>
             </router-link>
           </div>
-          <p class="page-font text-sm lg:text-base" v-if="mygenres.length === 0">ジャンルを追加してください</p>
+          <p class="page-font text-xs lg:text-base" v-if="mygenres.length === 0">ジャンルを追加してください</p>
           <div class="flex flex-col flex justify-between items-center">
             <div v-for="mygenre in mygenres" :key="mygenre.id">
-              <button class="page-font text-white text-sm lg:text-base bg-indigo-500 border rounded-full p-2" @click="fetchBoth(mygenre)">{{ mygenre.genre.name }}</button>
+              <button class="page-font text-white text-xs lg:text-base bg-indigo-500 border rounded-full p-2" @click="fetchBoth(mygenre)">{{ mygenre.genre.name }}</button>
             </div>
           </div>
         </div>
--- app/javascript/pages/post/new.vue
+++ app/javascript/pages/post/new.vue
@@ -89,8 +89,8 @@
                 </svg>
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
               </span>
-              <span class="page-font absolute flex items-center justify-center text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">マイページ</span>
-              <span class="page-font relative invisible">マイページ</span>
+              <span class="page-font absolute flex items-center justify-center text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">マイページへ</span>
+              <span class="page-font relative invisible">マイページへ</span>
             </div>
           </router-link>
         </div>
