e12559314c15e97f6201a045a1654388fa1cfd5c
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-10 21:37:34 +0900

    Add:Twitterログイン実装の準備

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  category   :integer          default("fashion"), not null
+#  image      :string
+#  title      :string
+#  type       :integer          default("twitter"), not null
+#  url        :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  mygenre_id :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_mygenre_id  (mygenre_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (mygenre_id => mygenres.id)
+#
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :mygenre_id, :category, :type, :url, :created_at, :title, :image, :tweet_url
 
