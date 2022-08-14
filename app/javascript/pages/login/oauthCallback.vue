4becf7575fe5c3eb0ef2bfb31d4f6f1c7da5658e
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-14 11:36:30 +0900

    Fix:Vue側のエラーが出ていたので修正

--- app/javascript/pages/login/oauthCallback.vue
+++ app/javascript/pages/login/oauthCallback.vue
@@ -15,8 +15,8 @@ export default {
         oauth_token: this.$route.query.oauth_token,
         oauth_verifier: this.$route.query.oauth_verifier,
       });
-      debugger;
-      this.$store.dispatch("loginUser", userResponse.data);
+      this.$store.dispatch("users/loginUser", userResponse.data);
+      this.$router.push({ name: "TopIndex" });
       this.$store.dispatch("setFlash", {
         type: "success",
         message: "ログインしました",
@@ -27,7 +27,6 @@ export default {
         message: "ログインに失敗しました。",
       });
     }
-    this.$router.push({ name: "TopIndex" });
   }
 };
 </script>
