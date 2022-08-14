a5490ec84bdb4f4b8e76195e4f4ada9b24a76076
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-14 10:08:15 +0900

    Fix:コントローラ処理修正

--- app/javascript/pages/login/oauthCallback.vue
+++ app/javascript/pages/login/oauthCallback.vue
@@ -15,19 +15,19 @@ export default {
         oauth_token: this.$route.query.oauth_token,
         oauth_verifier: this.$route.query.oauth_verifier,
       });
+      debugger;
       this.$store.dispatch("loginUser", userResponse.data);
       this.$store.dispatch("setFlash", {
         type: "success",
         message: "ログインしました",
       });
     } catch (error) {
-      console.log(error)
       this.$store.dispatch("setFlash", {
         type: "error",
         message: "ログインに失敗しました。",
       });
     }
     this.$router.push({ name: "TopIndex" });
-  },
+  }
 };
 </script>
