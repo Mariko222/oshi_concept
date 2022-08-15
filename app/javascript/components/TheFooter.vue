3821ccf85f9a97a69eaaeee8385314313f92f411
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-15 17:01:16 +0900

    Add:お問い合わせ先にTwitterアカウント追加

--- app/javascript/components/TheFooter.vue
+++ app/javascript/components/TheFooter.vue
@@ -6,7 +6,7 @@
     <div class="text-xs lg:text-base grid-flow-col gap-4 md:place-self-center md:justify-self-end lg:justify-self-end">
       <router-link :to="{ name: 'TheTerms' }" class="page-font text-xs lg:text-base">利用規約</router-link>
       <router-link :to="{ name: 'ThePolicy' }" class="page-font text-xs lg:text-base">プライバシーポリシー</router-link>
-      <a class="page-font" href="#">お問合わせ</a>
+      <a class="page-font" href="https://twitter.com/momo95843">お問い合わせ<br>(開発者Twitter)</a>
     </div>
   </footer>
 </template>
