2b7e14a8c7d938f70f710678a1494337ce03f9e5
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-17 18:05:09 +0900

    Add:メタタグ追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: '推し概念集めてみた',
+      title: '推し概念集めてみた',
+      description: '推しのイメージと感じたもの(概念）を好きなだけ集めてオリジナルのマイページを作成できます',
+      keywords: '推し, 概念, キャラ, キャラクター, アイドル, 概念コスメ, 概念コーデ, イメージ',
+      noindex: !Rails.env.production?,
+      canonical: request.original_url,
+      reverse: true,
+      charset: 'UTF-8',
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('apple-touch-icon.png'), rel: 'apple-touch-icon' }
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        creator: '@momo95843',
+        card: 'summary_large_image',
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
