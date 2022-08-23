1f8220f311453aefaf485ddcfebd5f51763fc96a
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-23 19:37:37 +0900

    Add:マイページヘッダーの共有するボタン実装

--- app/javascript/components/MypageHeader.vue
+++ app/javascript/components/MypageHeader.vue
@@ -13,30 +13,40 @@
           </router-link>
           <ul class="justify-center items-center md:flex">
             <li>
-              <button
-                class="page-font text-xs flex justify-center bg-twitter hover:bg-sky-400 active:bg-sky-500 text-white font-semibold text-center rounded-full transition duration-100 gap-2 mt-3 px-2 py-2"
+              <ShareNetwork
+                :url="'https://www.oshi-concept.com/mypage/'+ user.uuid"
+                :title="user.name + 'の推し概念集めてみた【' + user.mypage_name + '】を公開'"
+                description="推しの概念を集めたマイページを公開"
+                network="twitter"
+                hashtags="推し概念集めてみた"
               >
-                <svg class="w-5 h-5 shrink-0" width="24" height="24" viewBox="328 355 335 276" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="
-                    M 630, 425
-                    A 195, 195 0 0 1 331, 600
-                    A 142, 142 0 0 0 428, 570
-                    A  70,  70 0 0 1 370, 523
-                    A  70,  70 0 0 0 401, 521
-                    A  70,  70 0 0 1 344, 455
-                    A  70,  70 0 0 0 372, 460
-                    A  70,  70 0 0 1 354, 370
-                    A 195, 195 0 0 0 495, 442
-                    A  67,  67 0 0 1 611, 380
-                    A 117, 117 0 0 0 654, 363
-                    A  65,  65 0 0 1 623, 401
-                    A 117, 117 0 0 0 662, 390
-                    A  65,  65 0 0 1 630, 425
-                    Z"
-                    fill="white" />
-                </svg>
-                共有する
-              </button>
+                <div>
+                  <button
+                    class="page-font text-xs flex justify-center bg-twitter hover:bg-sky-400 active:bg-sky-500 text-white font-semibold text-center rounded-full transition duration-100 gap-2 mt-3 px-2 py-2"
+                  >
+                    <svg class="w-5 h-5 shrink-0" width="24" height="24" viewBox="328 355 335 276" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="
+                        M 630, 425
+                        A 195, 195 0 0 1 331, 600
+                        A 142, 142 0 0 0 428, 570
+                        A  70,  70 0 0 1 370, 523
+                        A  70,  70 0 0 0 401, 521
+                        A  70,  70 0 0 1 344, 455
+                        A  70,  70 0 0 0 372, 460
+                        A  70,  70 0 0 1 354, 370
+                        A 195, 195 0 0 0 495, 442
+                        A  67,  67 0 0 1 611, 380
+                        A 117, 117 0 0 0 654, 363
+                        A  65,  65 0 0 1 623, 401
+                        A 117, 117 0 0 0 662, 390
+                        A  65,  65 0 0 1 630, 425
+                        Z"
+                        fill="white" />
+                    </svg>
+                    共有する
+                  </button>
+                </div>
+              </ShareNetwork>
             </li>
           </ul>
         </div>
