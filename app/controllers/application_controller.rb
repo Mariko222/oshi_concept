5bfdace4c6b6a5d4ff3448c138ff0b3e5a0995cc
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-08-26 21:09:32 +0900

    Add:管理者ログインの権限周りの設定追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
   include Api::UserAuthenticator
 
   protect_from_forgery with: :null_session
-  
+
+  def not_authenticated
+    
+    # Make sure that we reference the route from the main app.
+    redirect_to main_app.admin_login_path, notice: "ログインしてください"
+  end
 end
