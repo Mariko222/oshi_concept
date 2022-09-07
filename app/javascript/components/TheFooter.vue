5b07c529bc03f8929e439dc4fcaf37f826389e7b
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-09-07 22:09:49 +0900

    Fix:フッターのレイアウト変更

--- app/javascript/components/TheFooter.vue
+++ app/javascript/components/TheFooter.vue
@@ -6,9 +6,9 @@
     <div class="text-xs lg:text-base grid-flow-col gap-4 md:place-self-center md:justify-self-end lg:justify-self-end">
       <router-link :to="{ name: 'TheTerms' }" class="page-font text-xs lg:text-base">利用規約</router-link>
       <router-link :to="{ name: 'ThePolicy' }" class="page-font text-xs lg:text-base">プライバシーポリシー</router-link>
-      <p class="page-font">お問い合わせ<br>
+      <p class="page-font">お問い合わせ
         <a href="https://twitter.com/momo95843">(開発者Twitter)</a>
-        <span>/</span><br>
+        <span>/</span>
         <a href="https://twitter.com/oshi_concept">(公式Twitter)</a>
       </p>
     </div>
