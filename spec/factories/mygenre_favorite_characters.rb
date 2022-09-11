e5eb1ee6e8cac8502fc58d00705f440c5a240e45
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-09-11 20:15:29 +0900

    Add:FactoryBotにデータ生成

---
+++ spec/factories/mygenre_favorite_characters.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :mygenre_favorite_character do
+    association :mygenre
+  end
+end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :post do
+    association :mygenre
+    category { "fashion" }
+    type { "twitter" }
+    url { "https://twitter.com/momo95843/status/1567372677010329600?s=20&t=9ii2Qew7HohjY3sdLOSSxA" }
+  end
+end
