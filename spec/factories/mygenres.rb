e6b5b1406b2f03e9be21dc9480304eaf85cd8303
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-09-12 23:58:12 +0900

    Add:ユーザー登録とログイン処理のテスト

--- spec/factories/mygenres.rb
+++ spec/factories/mygenres.rb
@@ -1,10 +1,6 @@
 FactoryBot.define do
   factory :mygenre do
     association :user
-
-    after(:create) do |mygenre|
-      create_list(:mygenre_favorite_character, 3, mygenre: mygenre)
-      genre_id { rand(1..5) }
-    end
+    genre_id { 1 }
   end
 end
