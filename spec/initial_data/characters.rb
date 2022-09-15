cb4da634719dce034bb79e11ad1b1421aa375422
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-09-15 17:36:55 +0900

    Add:ジャンルと推し登録処理のテスト追加

--- spec/initial_data/characters.rb
+++ spec/initial_data/characters.rb
@@ -2,4 +2,12 @@ Character.seed do |s|
   s.id = 1
   s.genre_id = 1
   s.name = '明星スバル'
+
+  s.id = 53
+  s.genre_id = 2
+  s.name = 'エース・トラッポラ'
+
+  s.id = 54
+  s.genre_id = 2
+  s.name = 'デュース・スペード'
 end
---
+++ spec/requests/mygenres_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "Mygenres", type: :request do
+  let(:mygenre) { create(:mygenre) }
+
+  context "GET /api/mypage" do
+    example "ログインしているユーザーのジャンルと推しを取得できる" do
+      token = login(mygenre.user)
+      get api_mypage_index_path(mygenre.user.uuid), headers: { Authorization: "Bearer #{token}" }
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  context "POST /api/mypage" do
+    example "ジャンルと推しを追加できる" do
+      token = login(mygenre.user)
+      post api_mypage_index_path, params: { genre_id: 2, character_ids: [54] }, headers: { Authorization: "Bearer #{token}" }
+      expect(response).to have_http_status(200)
+      expect(MygenreFavoriteCharacter.count).to eq(1)
+    end
+    example "既に登録済みのジャンルと推しを追加できない" do
+      token = login(mygenre.user)
+      post api_mypage_index_path, params: { genre_id: 1, character_ids: [1] }, headers: { Authorization: "Bearer #{token}" }
+      expect(response).to have_http_status(500)
+      expect(MygenreFavoriteCharacter.count).to eq(0)
+    end
+  end
+
+  context "PATCH /api/mypage/:id" do
+    example "ログインしているユーザーのマイページを編集できる" do
+      token = login(mygenre.user)
+      patch api_mypage_path(mygenre.user.id), params: { user: { name: "test2", mypage_name: "mypage2" } }, headers: { Authorization: "Bearer #{token}" }
+      expect(response).to have_http_status(200)
+    end
+  end
+end
