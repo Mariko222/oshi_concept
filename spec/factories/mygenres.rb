67aa69d36fa345131e42a1835f9e2f4e636dc28a
Mariko222 <mariko.yamamoto22gm@gmail.com>
2022-09-15 17:36:55 +0900

    Add:ジャンルと推し登録処理のテスト追加

--- spec/factories/mygenres.rb
+++ spec/factories/mygenres.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     id { SecureRandom }
     user_id { user.id }
     genre_id { 1 }
+
+    trait :with_mygenre_favorite_characters do
+      after(:create) { |mygenre| create_list(:mygenre_favorite_character, 3, mygenre: mygenre) }
+    end
   end
 end
--- spec/initial_data/genres.rb
+++ spec/initial_data/genres.rb
@@ -1,4 +1,7 @@
 Genre.seed do |s|
   s.id = 1
   s.name = 'あんさんぶるスターズ!!'
+
+  s.id = 2
+  s.name = "ディズニー ツイステッドワンダーランド"
 end
